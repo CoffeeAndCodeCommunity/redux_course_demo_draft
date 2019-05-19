0ff7127a51c56bd4c94512d5d320042361aa6d7f
Thomas <thomas@craftacademy.se>
2019-05-19 13:57:49 +0200

    adds chagee greeting functionality by adding a proposed greeting to application state and then changing the displayed state on button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import {Container, Header} from 'semantic-ui-react'
-import {connect} from 'react-redux';
+import { Container, Header, Button, Input } from 'semantic-ui-react'
+import { connect } from 'react-redux';
+
 
 const App = (props) => {
+
+  // const changeGreeting = () => {
+  //   props.dispatch({ type: 'CHANGE_GREETING' })
+  // }
+
   return (
     <>
       <Container>
         <Header as='h1'>{props.state.greeting}</Header>
+        <Input
+          placeholder='Greeting...'
+          onChange={(event) => props.dispatch({ type: 'SET_NEW_GREETING', greeting: event.target.value })}
+        />
+        <Button
+          primary
+          // onClick={changeGreeting.bind(this, 'text')}
+          onClick={() => props.dispatch({ type: 'CHANGE_GREETING', greeting: 'New greeting' })}
+        >
+          Change greeting
+        </Button>
       </Container>
     </>
   );
--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,7 +1,20 @@
 import initialState from '../store/initialState'
 
-const rootReducer = (state = initialState) => {
-  return state
+const rootReducer = (state = initialState, action) => {
+  if (action.type === 'CHANGE_GREETING') {
+    debugger
+    return {
+      ...state,
+      greeting: state.proposed_greeting
+    }
+  } else if (action.type === 'SET_NEW_GREETING') {
+    return {
+      ...state,
+      proposed_greeting: action.greeting
+    }
+  } else {
+    return state
+  }
 }
 
 export default rootReducer
